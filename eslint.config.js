339254f3e1a4fcef0b8ffc089cc361030cf1974d
v_zehongxu <v_zehongxu@tencent.com>
2025-06-23 16:33:01 +0800

    feat: 恢复babel，删除metro

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
-]);
+    expoConfig,
+    {
+        ignores: ['dist/*'],
+        parserOptions: {
+            ecmaVersion: 'latest', // 或具体版本号如 2022
+            sourceType: 'module', // 启用 ES 模块
+        },
+    },
+])
