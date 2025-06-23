2181842a37c08ba8d416c561696f8e6aa1a85c30
v_zehongxu <v_zehongxu@tencent.com>
2025-06-23 12:54:49 +0800

    reset-project

--- app/(tabs)/animes.tsx
+++ app/(tabs)/animes.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Text, View } from 'react-native'
 
-const index = () => {
+const animes = () => {
     return (
         <View>
-            <Text>index</Text>
+            <Text>animes</Text>
         </View>
     )
 }
 
-export default index
+export default animes
