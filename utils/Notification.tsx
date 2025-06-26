c124b654515ed9afc51ac8ecbb0442c36c60a7a6
天平 <1229248506@qq.com>
2025-06-27 02:50:14 +0800

    feat: 添加图片缓存

--- utils/Notification.tsx
+++ utils/Notification.tsx
@@ -1,9 +1,13 @@
 import { db } from '@/db'
 import { animeTable, insertAnimeSchema, selectAnimeSchema } from '@/db/schema'
 import dayjs from 'dayjs'
+import { Image } from 'expo-image'
 import React, { useEffect, useState } from 'react'
-import { Button, ScrollView, Text } from 'react-native'
+import { Button, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { sendNotification } from './notifications'
+
+const blurhash =
+    '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj['
 
 const Notification = () => {
     type TAnime = typeof animeTable.$inferInsert
@@ -49,6 +53,16 @@ const Notification = () => {
                     sendNotification('测试通知', '这是一个测试通知内容')
                 }}
             />
+            <View style={styles.container}>
+                <Image
+                    style={styles.image}
+                    source="https://picsum.photos/seed/696/3000/2000"
+                    placeholder={{ blurhash }}
+                    contentFit="cover"
+                    transition={1000}
+                    cachePolicy={'memory-disk'}
+                />
+            </View>
             <Button title="添加数据" onPress={insert} />
             {list.map((item) => {
                 return <Text key={item.id}>{item.createdAt && dayjs(item.createdAt * 1000).format('YYYY-MM-DD')}</Text>
@@ -58,3 +72,18 @@ const Notification = () => {
 }
 
 export default Notification
+
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        height: 300,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    image: {
+        flex: 1,
+        width: '100%',
+        backgroundColor: '#0553',
+    },
+})
