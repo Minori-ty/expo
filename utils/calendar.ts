6c27604c245752a9d67f752939a65f3e16436b29
天平 <1229248506@qq.com>
2025-07-01 00:25:20 +0800

    feat: 修复时间问题

--- utils/calendar.ts
+++ utils/calendar.ts
@@ -25,11 +25,15 @@ export async function createCalendarEvent({
         console.log('没有找到可修改的默认日历')
         return
     }
+    const day = dayjs.unix(firstEpisodeDateTime)
+    const weekday = day.isoWeekday()
+    const hours = day.hour()
+    const minutes = day.minute()
     // 解析输入的时间字符串
     const eventId = await Calendar.createEventAsync(defaultCalendar.id, {
         title: `${name} 第${currentEpisode + 1}集 即将更新!`,
-        startDate: dayjs.unix(firstEpisodeDateTime).toDate(),
-        endDate: dayjs.unix(firstEpisodeDateTime).toDate(),
+        startDate: dayjs().isoWeekday(weekday).hour(hours).minute(minutes).toDate(),
+        endDate: dayjs().isoWeekday(weekday).hour(hours).minute(minutes).toDate(),
         timeZone: 'Asia/Shanghai',
         alarms: [
             { relativeOffset: -5 }, // 提前10分钟通知
